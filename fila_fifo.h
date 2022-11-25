fa5125733d2450477a10740e5b90818543bc172a
R0dr1g0RS <116824752+R0dr1g0RS@users.noreply.github.com>
2022-11-25 14:32:41 -0300

    Update fila_fifo.h

--- fila_fifo.h
+++ fila_fifo.h
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+typedef struct Fifo
+{
+    int NumCon;
+    int NumCla; 
+    int QantOp;
+    struct Fifo *prox;
+} Fila_FIFO;
+
+void nome_arqvs(char *ent, char *sai, char *argv);
+
+void f_inicializar (Fila_FIFO **f);
+
+int f_inserir (Fila_FIFO **f, int chave, int valor);
+
+int f_num_elementos (Fila_FIFO **f);
