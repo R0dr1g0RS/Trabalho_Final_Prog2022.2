0698483f9144e3de5a3ff45bb985877a42a91c82
LucasSLopes <82237395+LucasSLopes@users.noreply.github.com>
2022-11-27 22:06:48 -0300

    Update escalonador.c

--- escalonador.c
+++ escalonador.c
@@ -633,5 +633,35 @@ o resultado do processamento para arquivo de nome “nome_arq_out”.
 */
 void e_rodar (Escalonador *e, char *nome_arq_in, char *nome_arq_out)
 {
+    FILE *entradas;
+    int caixa, tempo;
+    int e_1, e_2, e_3, e_4, e_5; //disciplina de atendimento para cada classe de conta;
+    int num_conta, num_operacoes;
+    char classe_conta[8];
 
+    entradas = fopen(arq_nome, "r");
+
+    if (entradas == NULL){
+        printf("entrou\n");
+        return 0;
+    }
+
+    fscanf(entradas, "qtde de caixas = %d\n", &caixa);
+    printf("%d\n", caixa);
+    fscanf(entradas, "delta t = %d\n", &tempo);
+    printf("%d\n", tempo);
+    fscanf(entradas,"disciplina de escalonamento = {%d,%d,%d,%d,%d}\n", &e_1, &e_2,&e_3, &e_4, &e_5);
+    printf("%d\n", e_1);
+    printf("%d\n", e_2);
+    printf("%d\n", e_3);
+    printf("%d\n", e_4);
+    printf("%d\n", e_5);
+    //Essa parte é só pra ver se tava pegando os arquivos corretamente;
+    //Tem que fazer ir alocando na fila de acordo com a disciplina de escalonamento;
+    while(fscanf(entradas,"%s - conta %d - %d operacao(oes)", classe_conta, &num_conta , &num_operacoes) != EOF){
+        printf("Classe:\t%s\n", classe_conta);
+        printf("Conta:\t%d\n", num_conta);
+        printf("Operações:\t%d\n", num_operacoes);
+    }
+    return 1;
 };
