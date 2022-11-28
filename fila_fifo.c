51e717a0e929a71a75e88efcc8b557357db148df
Guilherme G. Botelho <95437442+guimagedon@users.noreply.github.com>
2022-11-27 21:53:57 -0300

    Update fila_fifo.c

--- fila_fifo.c
+++ fila_fifo.c
@@ -4,18 +4,64 @@ void nome_arqvs(char *ent, char *sai, char *argv)
 {
     char nulo[4] = "0", *num_arq = (char*)malloc(sizeof(char) * 4);
     
-    while(strlen(nulo) + strlen(argv) < 4)
+    if(strlen(argv) == 4)
     {
-        strcat(nulo, "0");
+        strcat(ent, "entrada-");
+        strcat(ent, argv);
+        strcat(ent, ".txt");
+        strcat(sai, "saida-");
+        strcat(sai, argv);
+        strcat(sai, ".txt");
     }
-    strcat(nulo, argv);
-    strcpy(num_arq, nulo);
-    strcat(ent, "entrada-");
-    strcat(ent, num_arq);
-    strcat(ent, ".txt");
-    strcat(sai, "saida-");
-    strcat(sai, num_arq);
-    strcat(sai, ".txt");
+    else
+    {
+        while(strlen(nulo) + strlen(argv) < 4)
+        {
+            strcat(nulo, "0");
+        }
+        strcat(nulo, argv);
+        strcpy(num_arq, nulo);
+        strcat(ent, "entrada-");
+        strcat(ent, num_arq);
+        strcat(ent, ".txt");
+        strcat(sai, "saida-");
+        strcat(sai, num_arq);
+        strcat(sai, ".txt");
+    }
+}
+
+int ler_arq(char *arq_nome)
+{
+    FILE *arq;
+    int caixa, tempo;
+    int e_1, e_2, e_3, e_4, e_5; //disciplina de atendimento para cada classe de conta;
+    int num_conta, num_operacoes;
+    char classe_conta[8];
+    
+    arq = fopen(arq_nome, "rt");
+
+    if (arq == NULL){
+        printf("Nao foi possivel ler o arquivo\n");
+        return 0;
+    }
+
+    fscanf(arq, "qtde de caixas = %d\n", &caixa);
+    printf("Quantidade de Caixas: %d\n", caixa);
+    fscanf(arq, "delta t = %d\n", &tempo);
+    printf("DeltaT: %d\n", tempo);
+    fscanf(arq,"disciplina de escalonamento = {%d,%d,%d,%d,%d}\n", &e_1, &e_2,&e_3, &e_4, &e_5);
+    printf("E1: %d\n", e_1);
+    printf("E2: %d\n", e_2);
+    printf("E3: %d\n", e_3);
+    printf("E4: %d\n", e_4);
+    printf("E5: %d\n", e_5);
+
+    while(fscanf(arq,"%s - conta %d - %d operacao(oes)", classe_conta, &num_conta , &num_operacoes) != EOF){
+        printf("Classe: %s\n", classe_conta);
+        printf("Conta: %d\n", num_conta);
+        printf("Operações: %d\n", num_operacoes);
+    }
+    return 1;
 }
 
 void f_inicializar (Fila_FIFO **f)
