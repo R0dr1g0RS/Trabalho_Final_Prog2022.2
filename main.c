679484917f3282f619b358d9736d52521f60a65f
Guilherme G. Botelho <95437442+guimagedon@users.noreply.github.com>
2022-11-30 19:35:19 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include "fila_fifo.h"
+#include "escalonador.h"
 
 int main(int narg, char *argv[])
 {
@@ -6,47 +6,52 @@ int main(int narg, char *argv[])
     FILE *arq;
     Fila_FIFO *f1, *f2, *f3, *f4, *f5;
     char *nome_ent = (char*)malloc(sizeof(char) * 16), *nome_sai = (char*)malloc(sizeof(char) * 14);
+    char nulo[4] = "0", *num_arq = (char*)malloc(sizeof(char) * 4);
+    Escalonador *e;
     
-    nome_arqvs(nome_ent, nome_sai, argv[1]);
-    puts(nome_ent);             /*obtendo os nomes dos arquivos*/
-    puts(nome_sai);
-    
-    f_inicializar(&f1);
-    f_inicializar(&f2);
-    f_inicializar(&f3);           /*inicializando as filas*/
-    f_inicializar(&f4);
-    f_inicializar(&f5);
-    
-    //Ainda tem que ajeitar para funcionar aqui;
-    char 
-        nulo[4] = "0", 
-        sai[20], 
-        ent[20],
-        *num_arq = (char*)malloc(sizeof(char) * 4);
-
-    if(strlen(argv) == 4)
+    if(strlen(argv[1]) == 4)
     {
-        strcat(ent, "entrada-");
-        strcat(ent, argv);
-        strcat(ent, ".txt");
-        strcat(sai, "saida-");
-        strcat(sai, argv);
-        strcat(sai, ".txt");
+        strcat(nome_ent, "entrada-");
+        strcat(nome_ent, argv[1]);          
+        strcat(nome_ent, ".txt");
+        strcat(nome_sai, "saida-");       /*obtendo os nomes dos arquivos*/
+        strcat(nome_sai, argv[1]);
+        strcat(nome_sai, ".txt");
     }
     else
     {
-        while(strlen(nulo) + strlen(argv) < 4)
+        while(strlen(nulo) + strlen(argv[1]) < 4)
         {
             strcat(nulo, "0");
         }
-        strcat(nulo, argv);
+        strcat(nulo, argv[1]);
         strcpy(num_arq, nulo);
-        strcat(ent, "entrada-");
-        strcat(ent, num_arq);
-        strcat(ent, ".txt");
-        strcat(sai, "saida-");
-        strcat(sai, num_arq);
-        strcat(sai, ".txt");
+        strcat(nome_ent, "entrada-");
+        strcat(nome_ent, num_arq);
+        strcat(nome_ent, ".txt");
+        strcat(nome_sai, "saida-");
+        strcat(nome_sai, num_arq);
+        strcat(nome_sai, ".txt");
     }
+    //puts(nome_ent);
+    //puts(nome_sai);
+    //f_inicializar(&f1);
+    //f_inserir(&f1, 755816, 3);
+    //percorrer(f1);
+    
+    e_inicializar(e, 7, 5, 11, 10, 8, 5, 2);
+    if(e->premium == NULL)
+    {
+        printf("Sim");
+    }
+    else
+    {
+        printf("Nao");
+    }
+    
+    //e_inserir_por_fila(e, 1, 755816, 3);
+    //percorrer(e->premium);
+    
+    
     return EXIT_SUCCESS;
 }
