c9259224d1584c4f90b51cf60fda9e27cb4a6a9f
LucasSLopes <82237395+LucasSLopes@users.noreply.github.com>
2022-12-01 19:41:32 -0300

    Update logtree.c

--- logtree.c
+++ logtree.c
@@ -1,9 +1,4 @@
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "logtree.h"
-
 //Inicializa a árvore com o primeiro nó sendo NULL;
 void log_inicializar(Log **l){
     *l = NULL;
@@ -19,7 +14,7 @@ void log_registrar(Log **l, int conta, int classe, int timer, int caixa){
 
         novo_no->conta_num = conta;
         novo_no->conta_classe = classe;
-        novo_no->caixa_qntd = caixa;
+        novo_no->caixa_num = caixa;
         novo_no->timer = timer;
 
         novo_no->esquerda= NULL;
@@ -28,10 +23,10 @@ void log_registrar(Log **l, int conta, int classe, int timer, int caixa){
     }
     //Se a arvore não estiver vazia
     else{
-        if (classe < (*l)->conta_classe && (*l)->esquerda != NULL) //Se a classe do novo_nó for menor que a de *l e ele possuir filho esquerdo;
+        if (conta < (*l)->conta_num && (*l)->esquerda != NULL) //Se a classe do novo_nó for menor que a de *l e ele possuir filho esquerdo;
             log_registrar(&(*l)->esquerda, conta, classe, timer, caixa); //Chamar a função novamente no lado esquerdo de *l;
 
-        else if (classe > (*l)->conta_classe && (*l)->direita != NULL) //Se a classe do novo_nó for maior que a de *l e ele possuir filho direito;
+        else if (conta > (*l)->conta_num && (*l)->direita != NULL) //Se a classe do novo_nó for maior que a de *l e ele possuir filho direito;
             log_registrar(&(*l)->direita, conta, classe, timer, caixa); //Chamar a função novamente no lado direito de *l;
 
 
@@ -39,57 +34,52 @@ void log_registrar(Log **l, int conta, int classe, int timer, int caixa){
             novo_no = malloc(sizeof(Log));
             novo_no->conta_num = conta;
             novo_no->conta_classe = classe;
-            novo_no->caixa_qntd = caixa;
+            novo_no->caixa_num = caixa;
             novo_no->timer = timer;
 
             novo_no->esquerda= NULL;
             novo_no->direita = NULL;
 
-            if(classe < (*l)->conta_classe)  //Se a classe do novo_nó for menor que a de *l e ela não possuir filho esquerdo;
+            if(conta < (*l)->conta_num)  //Se a classe do novo_nó for menor que a de *l e ela não possuir filho esquerdo;
                 (*l)->esquerda = novo_no;
             else                             //Se a classe do novo_nó for maior que a de *l e ela não possuir filho direito;
                 (*l)->direita = novo_no;
+
         }
     }
-    
+
 }
 
 float log_media_por_classe(Log **l, int classe) {
-    float tempo_medio;
-    tempo_medio = log_obter_soma_por_classe(&(*l), classe)/ log_obter_contagem_por_classe(&(*l), classe);
+    float tempo_medio, tempo_total, qntd_contas;
+    tempo_total = log_obter_soma_por_classe(&(*l), classe);
+    qntd_contas = log_obter_contagem_por_classe(&(*l), classe);
+    tempo_medio = tempo_total/qntd_contas;
     //Soma do tempo de espera de uma classe dividido pela quantidade de contas dessa classe;
 
     return tempo_medio;
 }
 
 int log_obter_soma_por_classe(Log **l, int classe){
-    Log *raiz = (*l);
-    if(raiz == NULL){
+    int esq, dir;
+
+    if (*l == NULL){
         return 0;
-        //Se a raiz for nula então a árvore está vazia e não há elementos para contar;
     }
-    else {
-        if(raiz->conta_classe == classe) //Se a classe do nó for igual a classe especificada
-            return raiz->timer + log_obter_soma_por_classe(&raiz->esquerda, classe) + log_obter_soma_por_classe (&raiz->direita, classe);
-            //Soma o timer do nó atual com os retornos da chamada recursiva pelos lados esquerdo e direito da arvore;
-        else
-            return 0 + log_obter_soma_por_classe(&raiz->esquerda, classe) + log_obter_soma_por_classe (&raiz->direita, classe);
-            //Soma os retornos da chamada recursiva pelo lado esquerdo e direito da arvore;
-    }
+    esq = log_obter_soma_por_classe(&(*l)->esquerda,classe);
+    dir = log_obter_soma_por_classe(&(*l)->direita,classe);
+
+    return (*l)->timer + esq + dir;
 }
 
 int log_obter_contagem_por_classe(Log **l, int classe){
-    Log *raiz = (*l);
-    if(raiz == NULL){
+    int esq, dir;
+
+    if (*l == NULL){
         return 0;
-        //Se a raiz for nula então a árvore está vazia e não há elementos para contar;
     }
-    else {
-        if (raiz->conta_classe == classe) //Se a classe do nó for igual a classe especificada
-            return 1 + log_obter_contagem_por_classe(&raiz->esquerda, classe) + log_obter_contagem_por_classe(&raiz->direita, classe);
-            //Soma 1 com os retornos da chamada recursiva pelos lados esquerdo e direito da arvore;
-        else
-            return 0 + log_obter_contagem_por_classe(&raiz->esquerda, classe) + log_obter_contagem_por_classe(&raiz->direita, classe);
-            //Faz uma chamada recursiva, descendo pelos 2 lados da arvore, somando os retornos;
-    }
+    esq = log_obter_contagem_por_classe(&(*l)->esquerda,classe);
+    dir = log_obter_contagem_por_classe(&(*l)->direita,classe);
+
+    return 1 + esq + dir;
 }
