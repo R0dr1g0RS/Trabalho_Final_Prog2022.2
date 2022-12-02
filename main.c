07057a7d8ec9ceba82b09fa89d7b28fd0a26a666
LucasSLopes <82237395+LucasSLopes@users.noreply.github.com>
2022-12-02 17:09:35 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "escalonador.h"
 
-int main(int narg, char *argv[])
-{
-    int prox_chave, prox_valor, num_elementos;
-    FILE *arq;
-    Fila_FIFO *f1, *f2, *f3, *f4, *f5;
+
+int main(int argc, char * argv[]) {
+    struct escalonador e;
     char *nome_ent = (char*)malloc(sizeof(char) * 16), *nome_sai = (char*)malloc(sizeof(char) * 14);
     char nulo[4] = "0", *num_arq = (char*)malloc(sizeof(char) * 4);
-    Escalonador *e;
-    
+
     if(strlen(argv[1]) == 4)
     {
         strcat(nome_ent, "entrada-");
-        strcat(nome_ent, argv[1]);          
+        strcat(nome_ent, argv[1]);
         strcat(nome_ent, ".txt");
         strcat(nome_sai, "saida-");       /*obtendo os nomes dos arquivos*/
         strcat(nome_sai, argv[1]);
@@ -33,25 +33,8 @@ int main(int narg, char *argv[])
         strcat(nome_sai, num_arq);
         strcat(nome_sai, ".txt");
     }
-    //puts(nome_ent);
-    //puts(nome_sai);
-    //f_inicializar(&f1);
-    //f_inserir(&f1, 755816, 3);
-    //percorrer(f1);
     
-    e_inicializar(e, 7, 5, 11, 10, 8, 5, 2);
-    if(e->premium == NULL)
-    {
-        printf("Sim");
-    }
-    else
-    {
-        printf("Nao");
-    }
-    
-    //e_inserir_por_fila(e, 1, 755816, 3);
-    //percorrer(e->premium);
-    
-    
+    e_rodar(&e, nome_ent, nome_sai);    /*Rodar o programa*/
+
     return EXIT_SUCCESS;
 }
